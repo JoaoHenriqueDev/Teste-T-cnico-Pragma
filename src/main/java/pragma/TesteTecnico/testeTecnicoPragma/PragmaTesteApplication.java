c7f740cd7d9fb23f4e90f07307ef849a6e021d4f
João Henrique <139931834+bkr6@users.noreply.github.com>
2025-12-10 22:50:59 -0300

    Feat: criação do projeto do teste técnico da pragma

---
+++ src/main/java/pragma/TesteTecnico/testeTecnicoPragma/PragmaTesteApplication.java
@@ -0,0 +1,13 @@
+package pragma.TesteTecnico.testeTecnicoPragma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PragmaTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PragmaTesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pragma/TesteTecnico/testeTecnicoPragma/PragmaTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package pragma.TesteTecnico.testeTecnicoPragma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PragmaTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
